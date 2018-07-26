9c7158f15dfca697dc179d0086e3900df13b7209
cobunny <rrpixel@gmail.com>
2018-07-26 15:49:36 -0700

    Add components and CSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/layout/footer/footer.component';
+import { HomeComponent } from './home/home/home.component';
+import { NavbarComponent } from './shared/layout/navbar/navbar.component';
+import { FormsComponent } from './home/forms/forms.component';
+import { StatsComponent } from './home/stats/stats.component';
+import { ClientsComponent } from './home/clients/clients.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HomeComponent,
+    NavbarComponent,
+    FormsComponent,
+    StatsComponent,
+    ClientsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
